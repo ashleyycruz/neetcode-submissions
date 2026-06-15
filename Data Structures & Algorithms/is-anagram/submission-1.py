7b4d6d21615034f83751d8523988134092627a0f
Ashley Cruz-Lopez <ashleycruzlop3z@gmail.com>
2026-06-15 18:12:27 -0400

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,33 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        # hashmap 
+        # loop through s and make an hashmap 
+        # set up key for every letter 
+        if len(s) != len(t):
+            return False
+
+        count = {}
+
+        for char in s:
+            # adding/updating a key (incrementing count of a character)
+            count[char] = count.get(char, 0) + 1 
+
+        for char in t:
+            count[char] = count.get(char,0) - 1 # subtracting whats found
+            if count[char] < 0:  # if hits negs then ( less than 0)
+                return False # false 
+                
+        return True    
+
+
+
+
+            
+
+
+
+
+
+
+        
